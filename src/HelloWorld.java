dc5174a1ae231b6a11dfaf3a8b8eaab1b719e800
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-04-06 14:14:21 -0500

    syntax, types, and variables exercise

---
+++ src/HelloWorld.java
@@ -0,0 +1,74 @@
+public class HelloWorld {
+    public HelloWorld() {
+
+    }
+    public static void main(String[] args) { // main tab
+        System.out.println("Hello World");// sout tab
+
+//        int myFavoriteNumber = 22;
+//        System.out.println(myFavoriteNumber);
+//        String myString = "Sarah lost at foosball";
+//        System.out.println(myString);
+
+//        char myString = "Sarah lost at foosball"; //incompatible types
+//        System.out.println(myString);
+
+//        String myString = 3.14159; //incompatible types
+//        System.out.println(myString);
+
+//        long myNumber = ; // error, illegal start of expression
+//        System.out.println(myNumber);
+
+//        long myNumber = 3.14 ; // incompatible types
+//        System.out.println(myNumber);
+
+//        long myNumber = 123L; // Processes the code and outputs 123
+//        System.out.println(myNumber);
+
+//        long myNumber = 123; // Processes the code and outputs 123
+//        System.out.println(myNumber);
+
+//        int myNumber = 3.14; // Incompatible types
+//        System.out.println(myNumber);
+
+//        float myNumber = 3.14; // Incompatible types
+//        System.out.println(myNumber);
+
+        // increments the variable by one if called upon a second time
+//        int x = 5;
+//        System.out.println(x++); // Both will output 5 separately
+//        System.out.println(x); // with both, the output for the first is 5 and second is 6
+
+        // increments by one the first time, and then stays that number when called upon again
+//        int x = 5;
+//        System.out.println(++x); // first will output 6, second will output 5
+//        System.out.println(x); // with both, the output for both are 6
+
+//        int class = 23; // not a recognized statement
+//        System.out.println(class);
+
+//        String theNumberThree = "three"; // cannot be cast to class java integer
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+//        System.out.println(y);
+
+
+
+
+    }
+
+}
---
+++ src/SyntaxLecture.java
@@ -0,0 +1,49 @@
+public class SyntaxLecture {
+
+    public static void main(String[] args) {
+        /*Declaring variables
+        * without initializing*/
+        int weight;
+        // Initialization after declaration
+
+        weight = 12;
+        // Declare and initialize at once
+
+
+        String breed = "Chow Chow";
+        String name = "Belle";
+        int cuteness = 10;
+
+        // A statement:
+        // "JVM, do something!"
+        System.out.println(name + " is a " + breed);
+
+        // very small numvers: byte datatype
+        byte smol = 12;
+        System.out.println(smol);
+        // declare a long: can hold very big numbers
+        long bigNumber;
+        // assign the small value to the large variable
+        // "pour the small bucket into the large bucket"
+        bigNumber = smol;
+        System.out.println(bigNumber);
+        //assign a big value to the big bucket
+        bigNumber = 900000000000L;
+        // This won't work assigning a big value to a smaller type
+//        smol =  (byte) bigNumber;
+        // casting: converting one data type into another
+        smol = (byte) bigNumber;
+        System.out.println(smol);
+
+        // float: 4 bytes of storage space
+        float imafloat;
+        // double: 8 bytes of storage space
+        double imadouble = 9.0123456789012;
+        // I can't fit a double inside a float
+//        imafloat = imadouble;
+        System.out.println(imadouble);
+        imafloat = (float) imadouble;
+        System.out.println(imafloat);
+    }
+
+}
