38abc9bee40798fa7aa354eb974443b6895d1379
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-04-17 09:10:30 -0500

    Last minute edits

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,21 +1,38 @@
 package shapes;
 
+import util.Input;
+
 public class Circle {
-    private double radius;
 
-    public Circle(double radius) {
-        this.radius = radius;
-    }
+        public static int totalCircles;
 
-    public double getRadius() {
-        return radius;
-    }
+        private double radius;
 
-    public void setRadius(double radius) {
-        this.radius = radius;
-    }
+        public double getRadius() {
+            return radius;
+        }
 
-    public double getArea() {
-        return Math.PI * (radius * radius);
-    }
+        public void setRadius(double radius) {
+            this.radius = radius;
+        }
+
+        public double getArea(){
+            return Math.PI * radius * radius;
+        }
+
+        public double getCircumference(){
+            return 2 * Math.PI * radius;
+        }
+
+        public static void outputCircleInfo(){
+            double radius = new Input().getDouble("Please enter the radius of the circle:");
+            shapes.Circle circle = new shapes.Circle(radius);
+            System.out.printf("The area of your circle is: %.2f%n", circle.getArea());
+            System.out.printf("The circumference of your circle is: %.2f%n", circle.getCircumference());
+        }
+
+        public Circle(double radius) {
+            this.radius = radius;
+            totalCircles++;
+        }
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -2,12 +2,14 @@ package shapes;
 import util.*;
 public class CircleApp {
 
-    public static void main(String[] args) {
-        double radius = new Input().getDouble("Please enter the radius of the circle:");
-        Circle circle = new Circle(radius);
-        System.out.println("The area of your circle is: " + circle.getArea());
-
-
-    }
+        public static void main(String[] args) {
+            Input input = new Input();
+            String userChoice = "y";
+            while (input.yesNo(userChoice)) {
+                Circle.outputCircleInfo();
+                userChoice = input.getString("Would you like to create another circle?");
+            }
+            System.out.println(Circle.totalCircles);
+        }
 
 }
